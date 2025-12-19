baf92c9057b790897072c07cd3bda472cc905dd3
nasserallah hourichi <nasserallah.hourichi@gmail.com>
2025-12-19 21:20:24 +0100

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import './theme/theme.css';
-import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/website">
       <App />
-    </BrowserRouter>
   </React.StrictMode>,
 );
